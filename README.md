bed4328439cec862184b85597350e35586484e35
wrench1815 <hardeepkumar1815@gmail.com>
2021-10-19 21:25:19 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Genshin Builds Backend
+
+API based backend for Genshin Builds using STrapi CMS.
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '25981ee1-dece-46ca-b32e-204093388d35'
+};
